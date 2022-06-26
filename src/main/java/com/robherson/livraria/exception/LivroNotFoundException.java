f41f331aaa064113f916d3b282233e2301dd7105
Robherson Wector <robhersonwector@gmail.com>
2022-06-26 17:38:32 -0300

    refactor(livros): Correção no extends

--- src/main/java/com/robherson/livraria/exception/LivroNotFoundException.java
+++ src/main/java/com/robherson/livraria/exception/LivroNotFoundException.java
@@ -4,6 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Livro Não encontrado!")
-public class LivroNotFoundException extends Exception {
+public class LivroNotFoundException extends RuntimeException {
     
 }
