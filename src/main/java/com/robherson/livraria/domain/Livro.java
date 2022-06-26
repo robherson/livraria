4f5bae36086fcb2e0ef70d8ce8bb85d0e05dc6b9
Robherson Wector <robhersonwector@gmail.com>
2022-06-26 14:15:36 -0300

    refactor(livros): Adição da anotação Builder na Classe Livro

--- src/main/java/com/robherson/livraria/domain/Livro.java
+++ src/main/java/com/robherson/livraria/domain/Livro.java
@@ -4,6 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +13,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 @Document(collation = "livros")
 public class Livro {
 
