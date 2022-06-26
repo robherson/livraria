ad35e3bf85bcf43fbc8eea69324bc9aedcb71bbf
Robherson Wector <robhersonwector@gmail.com>
2022-06-26 18:00:27 -0300

    refactor(livros): Correção no texto de LivroAlugadoException

--- src/main/java/com/robherson/livraria/exception/LivroAlugadoException.java
+++ src/main/java/com/robherson/livraria/exception/LivroAlugadoException.java
@@ -3,7 +3,7 @@ package com.robherson.livraria.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Livro já está alugado!")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Livro está alugado!")
 public class LivroAlugadoException extends RuntimeException{
     
 }
