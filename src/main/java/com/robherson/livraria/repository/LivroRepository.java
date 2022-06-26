7cabeb0a05d5eaca434e09cd8311ae21af800d09
Robherson Wector <robhersonwector@gmail.com>
2022-06-26 14:40:33 -0300

    refactor(livros): Correção em LivroRepository

--- src/main/java/com/robherson/livraria/repository/LivroRepository.java
+++ src/main/java/com/robherson/livraria/repository/LivroRepository.java
@@ -1,9 +1,11 @@
 package com.robherson.livraria.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.robherson.livraria.domain.Livro;
 
+@Repository
 public interface LivroRepository extends MongoRepository<Livro, String> {
     
 }
